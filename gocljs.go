6e7fc0a60fc033f191ed2cf6c8fe67fdfaca3c6b
Bader Szabolcs <sb@baader.hu>
2020-09-16 16:49:04 +0200

    external html

--- gocljs.go
+++ gocljs.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	//"fmt"
+        "net/http"
+        "html/template"
 )
 
 func main() {
 	// hello := "hello yu bn"
 	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, `<!doctype html>
+	/*	fmt.Fprintf(w, `<!doctype html>
         <html>
         <head><title>Browser Starter</title></head>
         <link rel="stylesheet" type="text/css" href="/public/main.css">
@@ -20,7 +21,9 @@ func main() {
         <script type="text/javascript"  src="/public/main.js"></script>
         <script>starter.browser.init();</script>
         </body>
-        </html>`)
+        </html>`)*/
+        t, _ := template.ParseFiles("public/index.html")
+        t.Execute(w, nil)
 
 	})
 
