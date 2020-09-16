9b69991b1838c51f06ed49b208a2bcb946a2fa7a
Bader Szabolcs <sb@baader.hu>
2020-09-16 16:50:02 +0200

    external html /clean

--- gocljs.go
+++ gocljs.go
@@ -1,29 +1,31 @@
 package main
 
 import (
-	//"fmt"
-        "net/http"
-        "html/template"
+	// "fmt"
+	"html/template"
+	"net/http"
 )
 
 func main() {
 	// hello := "hello yu bn"
 	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	/*	fmt.Fprintf(w, `<!doctype html>
-        <html>
-        <head><title>Browser Starter</title></head>
-        <link rel="stylesheet" type="text/css" href="/public/main.css">
-        <body>
-        <h1>shadow-cljs - Browser</h1>
-        <div id="app"></div>
-        
-        <script type="text/javascript"  src="/public/main.js"></script>
-        <script>starter.browser.init();</script>
-        </body>
-        </html>`)*/
-        t, _ := template.ParseFiles("public/index.html")
-        t.Execute(w, nil)
+
+		/*	fmt.Fprintf(w, `<!doctype html>
+			<html>
+			<head><title>Browser Starter</title></head>
+			<link rel="stylesheet" type="text/css" href="/public/main.css">
+			<body>
+			<h1>shadow-cljs - Browser</h1>
+			<div id="app"></div>
+
+			<script type="text/javascript"  src="/public/main.js"></script>
+			<script>starter.browser.init();</script>
+			</body>
+			</html>`)*/
+
+		t, _ := template.ParseFiles("public/index.html")
+		t.Execute(w, nil)
 
 	})
 
