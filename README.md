d9e912ae479b88bab6627a84622a1d6ae9fab438
Bader Szabolcs <sb@baader.hu>
2020-09-17 11:01:58 +0200

    gracefully shutdonw clojurescript with go done

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # golang-clojurescript
-Test golang and clojurescript
+Gracefully shutdown Clojurescript (basic shadow-cljs project) with Golang
 
-Test with 
-```go run gocljs.go```
+Based on https://github.com/shadow-cljs/quickstart-browser shadow-cljs basic example. Detailed info about this project you can find on the link.
 
-ToDo .. create a nice ReadMe file..
+Test with  '`go run gocljs.go`' or '`go build .`' and run the compiled file. We use external files, but possible compile into binary the css/js/html files. 
+
+This basic example show how to shut down gracefully the cljs projects with Go, based on https://medium.com/@pinkudebnath/graceful-shutdown-of-golang-servers-using-context-and-os-signals-cc1fa2c55e97. That is possible with js/process `(.on js/process "SIGINT" shutdown-gracefully-functions)` and Clojure side too (https://medium.com/helpshift-engineering/achieving-graceful-restarts-of-clojure-services-b3a3b9c1d60d).
+
--- gocljs.go
+++ gocljs.go
@@ -1,33 +1,88 @@
 package main
 
 import (
-	// "fmt"
+	"context"
 	"html/template"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
+func serve(ctx context.Context) (err error) {
+
+	mux := http.NewServeMux()
+	mux.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+	mux.Handle("/", http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+
+			t, _ := template.ParseFiles("public/index.html")
+
+			http.SetCookie(w,
+				&http.Cookie{
+					Domain:   "localhost",
+					Expires:  time.Date(2020, 11, 23, 1, 5, 3, 0, time.UTC),
+					HttpOnly: true,
+					Name:     "main",
+					Path:     "/",
+					Value:    "this-is-my-cookie-value-not-hard-to-decrypt",
+					Secure:   false,
+				})
+
+			t.Execute(w, nil)
+		},
+	))
+
+	srv := &http.Server{
+		Addr:    ":6969",
+		Handler: mux,
+	}
+
+	go func() {
+		if err = srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen:%+s\n", err)
+		}
+	}()
+
+	log.Printf("server started")
+
+	<-ctx.Done()
+
+	log.Printf("server stopped")
+
+	ctxShutDown, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		cancel()
+	}()
+
+	if err = srv.Shutdown(ctxShutDown); err != nil {
+		log.Fatalf("server Shutdown Failed:%+s", err)
+	}
+
+	log.Printf("server exited properly")
+
+	if err == http.ErrServerClosed {
+		err = nil
+	}
+
+	return
+}
+
 func main() {
-	// hello := "hello yu bn"
-	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-		/*	fmt.Fprintf(w, `<!doctype html>
-			<html>
-			<head><title>Browser Starter</title></head>
-			<link rel="stylesheet" type="text/css" href="/public/main.css">
-			<body>
-			<h1>shadow-cljs - Browser</h1>
-			<div id="app"></div>
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
 
-			<script type="text/javascript"  src="/public/main.js"></script>
-			<script>starter.browser.init();</script>
-			</body>
-			</html>`)*/
+	ctx, cancel := context.WithCancel(context.Background())
 
-		t, _ := template.ParseFiles("public/index.html")
-		t.Execute(w, nil)
+	go func() {
+		oscall := <-c
+		log.Printf("system call:%+v", oscall)
+		cancel()
+	}()
 
-	})
-
-	http.ListenAndServe(":9000", nil)
+	if err := serve(ctx); err != nil {
+		log.Printf("failed to serve:+%v\n", err)
+	}
 }
